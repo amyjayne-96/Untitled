5b0c0503b378b64dbb20224bbc1afadbd1a3a3a0
amy <amyjayne-96@hotmail.co.uk>
2016-10-24 15:21:13 +0100

    dataconnect

--- dataconnect.php
+++ dataconnect.php
@@ -1,12 +1,11 @@
 <?php
 // connect to csdm- webdev server and sel ect database
-$db = new mysqli(
-'eu-cdbr-azure-north-e.cloudapp.net',
+$db = new mysqli
+('eu-cdbr-azure-north-e.cloudapp.net',
 'b6eddf165f7163',
 'b43888f8',
-'ajw1405437',
-);
+'ajw1405437');
 // test if connection was established, and print any errors
 if($db->connect _errno){
-    die(‘Connectfailed['.$db->connect _error.']');
+    die("Connectfailed:".$db->connect _error.);
 }

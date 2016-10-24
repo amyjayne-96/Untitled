36224665a7b55d718d36de11ebc49731748b64f8
amy <amyjayne-96@hotmail.co.uk>
2016-10-24 14:05:59 +0100

    dataconnect

--- dataconnect.php
+++ dataconnect.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1405437
- * Date: 24/10/2016
- * Time: 14:02
- */
+// connect to csdm- webdev server and sel ect database
+$db = new mysqli(
+‘ ‘eu-cdbr-azure-north-e.cloudapp.net’ ’,
+‘ ‘b6eddf165f7163’ ’,
+‘ ‘b43888f8’ ’,
+‘ ‘ajw1405437’ ’
+);
+// test if connection was established, and print any errors
+i f($db->connect _errno){
+    di e(‘Connectfailed['.$db->connect _error.']');
+}
